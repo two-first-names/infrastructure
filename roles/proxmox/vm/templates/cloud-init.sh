62c911f9281fde578f84805d9f79a85d9381888a
Joe Roberts <joe@engiqueer.net>
2024-04-08 22:00:10 +0100

    Initial commit

---
+++ roles/proxmox/vm/templates/cloud-init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+wget https://github.com/smallstep/cli/releases/download/v0.26.0/step_linux_amd64.tar.gz
+tar -xzf step_linux_amd64.tar.gz
+mv step_linux_amd64/bin/step /usr/bin/
+
+step ca bootstrap --ca-url https://ca.engiqueer.net/ --fingerprint {{ step_ca_fingerprint }}
+
+step ssh config --roots > /etc/ssh/ssh_user_key.pub
+step ssh certificate --token {{ token.stdout}} --host --sign {{ hostname }} /etc/ssh/ssh_host_ecdsa_key.pub
+
+cat <<EOF > /etc/ssh/sshd_config.d/ssh_ca.conf
+TrustedUserCAKeys /etc/ssh/ssh_user_key.pub
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostCertificate /etc/ssh/ssh_host_ecdsa_key-cert.pub
+EOF
+
+systemctl restart sshd
